f764a8a8a791e3f9a53e991f10393661a01ac798
kvguarin <kvguarin@dons.usfca.edu>
2019-01-06 19:15:55 -0800

    successfully print out times each guard was alseep in minutes, along with the number of times that guard was alseep each minute

--- src/DayFour.java
+++ src/DayFour.java
@@ -3,7 +3,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.spi.FileSystemProvider;
 import java.util.Map.Entry;
+
+import javax.sound.midi.VoiceStatus;
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+import org.omg.CORBA.portable.ValueBase;
+
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -67,9 +75,9 @@ public class DayFour {
 	 */
 	public void createGuardMap() {
 		// go through each entry in the chronMap
-		int guard; // guard number
-		int fallsAsleep; // time guard falls asleep
-		int wakesUp; // time guard wakes up
+		int guard = 0; // guard number
+		int fallsAsleep = 0; // time guard falls asleep
+		int wakesUp = 0; // time guard wakes up
 		String[] value;
 
 		for (Map.Entry<String, String> entry : chronMap.entrySet()) {
@@ -80,20 +88,89 @@ public class DayFour {
 				value = entry.getValue().replaceAll("#", "").split(" ");
 				guard = Integer.parseInt(value[2]);
 
-				
 			} else if (entry.getValue().startsWith(" falls")) {
-				//time guard falls asleep
+				// time guard falls asleep
 				System.out.println(entry.getKey());
-				System.out.println(entry.getKey().substring(entry.getKey().length()-3, entry.getKey().length()-1));
-				fallsAsleep = Integer.parseInt(entry.getKey().substring(entry.getKey().length()-3, entry.getKey().length()-1));
-				
-			} else { 
-				//time guard wakes up Save this onto the tree
+				System.out.println(entry.getKey().substring(entry.getKey().length() - 3, entry.getKey().length() - 1));
+				fallsAsleep = Integer
+						.parseInt(entry.getKey().substring(entry.getKey().length() - 3, entry.getKey().length() - 1));
+
+			} else {
+				// time guard wakes up Save this onto the tree
 				System.out.println(entry.getValue());
-				wakesUp = Integer.parseInt(entry.getKey().substring(entry.getKey().length()-3, entry.getKey().length()-1));
-				
+				wakesUp = Integer
+						.parseInt(entry.getKey().substring(entry.getKey().length() - 3, entry.getKey().length() - 1));
+				// TODO: save value into tree
+				insertSleepTime(guard, fallsAsleep, wakesUp);
 			}
 		}
+
+		return;
+
+	}
+
+	/*
+	 * Inserts the times that the guard was asleep into the map. If the guard
+	 * has already fallen asleep during those times, then increments the amount
+	 * of times said guard was asleep at that time by 1
+	 */
+	private void insertSleepTime(int guard, int fallsAsleep, int wakesUp) {
+
+		// Check if map contains guard already
+		if (guardMap.containsKey(guard)) {
+			// add sleep times into map
+
+			Map<Integer, Integer> minutesAsleep = guardMap.get(guard);
+
+			// for (Map.Entry<Integer, Integer> entry :
+			// minutesAsleep.entrySet()){
+			//
+			// }
+			for (int i = fallsAsleep; i < wakesUp; i++) {
+				if (minutesAsleep.containsKey(i)) {
+					int count = minutesAsleep.get(i);
+					minutesAsleep.put(i, count + 1);
+				} else {
+					minutesAsleep.put(i, 1);
+				}
+			}
+
+			guardMap.put(guard, minutesAsleep);
+
+		} else {
+			// add new guard and sleep times
+			// guardMap.
+			Map<Integer, Integer> minutesAsleep = new HashMap<>();
+			for (int i = fallsAsleep; i < wakesUp; i++) {
+				minutesAsleep.put(i, 1);
+			}
+			guardMap.put(guard, minutesAsleep);
+
+		}
+		return;
+	}
+
+	/*
+	 * Prints out the times the guard fell asleep
+	 */
+	public void printGuardMap() {
+
+		for (Map.Entry<Integer, Map<Integer, Integer>> entry : guardMap.entrySet()) {
+			for (Map.Entry<Integer, Integer> entry2 : entry.getValue().entrySet()) {
+				System.out.print("Guard: " + entry.getKey() + " ");
+				System.out.print("Minute: " + entry2.getKey() + " ");
+				System.out.println("Times Asleep: " + entry2.getValue());
+			}
+			System.out.println();
+		}
+		return;
+	}
+
+	/*
+	 * Finds the guard that slept the most, Along with the most likely time the
+	 * guard will be asleep
+	 */
+	public void findLazyGuard() {
 
 	}
 
--- src/Driver.java
+++ src/Driver.java
@@ -97,6 +97,7 @@ public class Driver {
 		File file = new File("/Users/KyVGuarin/Documents/Programming/AdventOfCode2018/TestInputs/DayFourInputTest1");
 		dayFour.organizeFile(file);
 		dayFour.createGuardMap();
+		dayFour.printGuardMap();
 		// TODO: make another map that is organized by guard Number and sleep
 		// times
 		// From there, calculate the guard who has slept the most
